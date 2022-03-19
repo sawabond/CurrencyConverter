e8440c2241e0f5e25e677847e617e418046e0d23
Ruslan <ruslan.tretyakov2003@gmail.com>
2022-03-19 15:09:10 +0200

    [CUR-10] Created class for API object

---
+++ WebAPI/CurrencyConverter/DTO/CurrencyDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CurrencyConverter.DTO
+{
+    public class CurrencyDto
+    {
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        public Dictionary<string, double> Rates { get; set; }
+    }
+}
